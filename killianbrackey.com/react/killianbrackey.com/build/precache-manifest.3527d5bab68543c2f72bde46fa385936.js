e2b55aa43c29714f724149c8ad26fecc4f9ef152
Killian Brackey <killian.brackey@sezzle.com>
2019-01-12 14:00:47 -0600

    Added build files for react version of site

---
+++ killianbrackey.com/react/killianbrackey.com/build/precache-manifest.3527d5bab68543c2f72bde46fa385936.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f2218e59ffbf3f33d707",
+    "url": "/static/js/main.f2218e59.chunk.js"
+  },
+  {
+    "revision": "59bf5cd8e9daa878cf29",
+    "url": "/static/js/1.59bf5cd8.chunk.js"
+  },
+  {
+    "revision": "f2218e59ffbf3f33d707",
+    "url": "/static/css/main.eaafe185.chunk.css"
+  },
+  {
+    "revision": "ecaef6bfca548f8c39d0a9c55ba18022",
+    "url": "/index.html"
+  }
+];
